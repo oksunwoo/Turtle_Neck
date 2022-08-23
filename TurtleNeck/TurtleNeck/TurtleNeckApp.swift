aed6d44809090af04a5b0bfe2b48bd30be5be906
oksunwoo <oksunwo@naver.com>
2022-08-23 18:53:58 +0900

    프로젝트 생성

---
+++ TurtleNeck/TurtleNeck/TurtleNeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurtleNeckApp.swift
+//  TurtleNeck
+//
+//  Created by Sunwoo on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TurtleNeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
