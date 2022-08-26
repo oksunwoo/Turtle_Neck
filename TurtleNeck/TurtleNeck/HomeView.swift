b634f1ba6f0a0c13be67aeda6fa32afb55c9394b
oksunwoo <oksunwo@naver.com>
2022-08-26 17:17:06 +0900

    test: Test를 위한 HomeView와 AccountView 생성

---
+++ TurtleNeck/TurtleNeck/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  TurtleNeck
+//
+//  Created by Sunwoo on 2022/08/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Home View")
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
