a32b6a9068110e5f35d6ac3b235a99da42278aaa
zoe <kny0651@naver.com>
2022-09-18 18:03:14 +0900

    feat: 온보딩아이템 이미지 및 타이틀 추가
    - 온보딩뷰에 로고이미지 추가
    - 둘러보기버튼 우측하단으로 이동

--- TurtleNeck/TurtleNeck/OnboardingView.swift
+++ TurtleNeck/TurtleNeck/OnboardingView.swift
@@ -14,29 +14,22 @@ struct OnboardingView: View {
     
     var body: some View {
         VStack() {
-            HStack {
-                Spacer()
-                Button {
-                    isFirstLaunching = false
-                    presentationMode.wrappedValue.dismiss()
-                } label: {
-                    Text("둘러보기")
-                        .font(.body)
-                        .foregroundColor(.white)
-                        .underline()
-                }
-                .padding()
+            HStack(alignment: .center) {
+            Image("LaunchScreenLogo")
+                .resizable()
+                .frame(width: 300, height: 50)
+                .padding(.top, 60)
             }
+           
             TabView {
-                OnboardingItem()
-                OnboardingItem()
-                OnboardingItem()
+                OnboardingItem(image: "tortoise.fill", title: "당신은 거북이")
+                OnboardingItem(image: "person.fill.questionmark", title: "인가요?")
+                OnboardingItem(image: "hand.point.right.fill", title: "알아보러 갑시다")
             }
             .tabViewStyle(.page)
             .indexViewStyle(.page(backgroundDisplayMode: .always))
             
             VStack(spacing: 20) {
-                
                 Button {
                     if (UserApi.isKakaoTalkLoginAvailable()) {
                         // 카카오톡 설치
@@ -88,6 +81,19 @@ struct OnboardingView: View {
                         .aspectRatio(contentMode: .fit)
                         .padding(.vertical, 0)
                 }
+            }
+            HStack {
+                Spacer()
+                Button {
+                    isFirstLaunching = false
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("둘러보기")
+                        .font(.body)
+                        .foregroundColor(.white)
+                        .underline()
+                }
+                .padding()
             }
         }
         .background(Color("MainGreenColor"))
