518ff32515fe64f9b1ea82cb52cfd9619e5f1be3
zoe <kny0651@naver.com>
2022-09-19 18:19:29 +0900

    feat: Setting을 enum으로 관리 및 프로필뷰 생성
    - 환경설정화면에서 유저의 프로필을 관리하도록 유저프로필버튼 추가
    - NavigationLink의 위치 SettingsView -> SettingsRow로 변경

--- TurtleNeck/TurtleNeck/SettingsView.swift
+++ TurtleNeck/TurtleNeck/SettingsView.swift
@@ -7,17 +7,36 @@
 
 import SwiftUI
 
+enum Setting: String {
+    case notice = "공지사항"
+    case customerService = "1:1 문의"
+    case version = "버전 정보"
+}
+
 struct SettingsView: View {
+    @State private var showingProfile = false
+    
     var body: some View {
         NavigationView {
             List {
-                NavigationLink {
-                    SettingsDetail()
-                } label: {
-                    SettingsRow(title: "DD")
-                }
-
+                SettingsRow(setting: .notice)
+                SettingsRow(setting: .customerService)
+                SettingsRow(setting: .version)
             }
+            .toolbar {
+                HStack {
+                    Spacer()
+                    Button {
+                        showingProfile.toggle()
+                    } label: {
+                        Label("User Profile", systemImage: "person.crop.circle")
+                    }
+                }
+            }
+            .navigationTitle("Settings")
+            .fullScreenCover(isPresented: $showingProfile, content: {
+                ProfileView(showingProfile: $showingProfile)
+            })
         }
     }
 }
