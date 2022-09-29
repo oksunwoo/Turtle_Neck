390868cb55aee4c2bae903f9c9228b4eaf13442d
zoe <kny0651@naver.com>
2022-09-29 16:13:10 +0900

    feat: 이미지 피커 추가

---
+++ TurtleNeck/TurtleNeck/Utility/ImagePicker.swift
@@ -0,0 +1,40 @@
+//
+//  ImagePicker.swift
+//  TurtleNeck
+//
+//  Created by 권나영 on 2022/09/28.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            self.parent.image = image
+            self.parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    }
+}
