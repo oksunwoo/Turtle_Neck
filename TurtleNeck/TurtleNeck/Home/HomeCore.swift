ffa5c821bef4a46d055dd43faadaa194a6290ef9
zoe <kny0651@naver.com>
2022-11-22 16:18:58 +0900

    refactor: TCA로 구조변경(1)
    - 앱확장성을 고려해 MV -> TCA구조로 아키텍쳐변경
    - 루트뷰를 위한 RootCore 추가

---
+++ TurtleNeck/TurtleNeck/Home/HomeCore.swift
@@ -0,0 +1,12 @@
+//
+//  HomeCore.swift
+//  TurtleNeck
+//
+//  Created by 권나영 on 2022/11/22.
+//
+
+import ComposableArchitecture
+
+struct HomeState: Equatable {
+
+}
---
+++ TurtleNeck/TurtleNeck/Settings/SettingsCore.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsCore.swift
+//  TurtleNeck
+//
+//  Created by 권나영 on 2022/11/22.
+//
+
+import ComposableArchitecture
+
+struct SettingState: Equatable {
+
+}
