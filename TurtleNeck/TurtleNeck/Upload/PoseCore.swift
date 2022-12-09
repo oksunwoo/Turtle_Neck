6be709b2949b3fe2fe7004fce31304dba1831f7f
oksunwoo <oksunwo@naver.com>
2022-12-09 23:00:37 +0900

    refactor: PoseCore action 및 reducer 추가

--- TurtleNeck/TurtleNeck/Upload/PoseCore.swift
+++ TurtleNeck/TurtleNeck/Upload/PoseCore.swift
@@ -12,24 +12,53 @@ struct PoseCore: ReducerProtocol {
     struct State: Equatable {
         @BindableState var selectedImage: UIImage?
         @BindableState var isImagePickerPresented = false
-        var photoData: Data?
-        var isPhotoRequest = false
+        var imageData: Data?
+        var isPoseRequest = false
+        var tempResult: String
     }
 
     enum Action: BindableAction, Equatable {
+        case confirmButtonTapped
+        case inputSelectedImage(UIImage?)
         case binding(BindingAction<State>)
         case showImagePicker
+        case poseResponse(TaskResult<String>)
     }
+    
+    @Dependency (\.poseClient) var poseClient
     
     var body: some ReducerProtocol<State, Action> {
         BindingReducer()
         Reduce { state, action in
             switch action {
-            case .binding(_):
+            case .confirmButtonTapped:
+                state.isPoseRequest = true
+                state.imageData = state.selectedImage?.jpegData(compressionQuality: 1)
+                
+                return .task { [imageData = state.imageData] in
+                    await .poseResponse(TaskResult {
+                        try await self.poseClient.fetch(imageData ?? Data())
+                    })
+                }
+                
+            case .inputSelectedImage(let image):
+                state.selectedImage = image
+                return .none
+                
+            case .binding:
                 return .none
                 
             case .showImagePicker:
                 state.isImagePickerPresented.toggle()
+                return .none
+                
+            case .poseResponse(.success(let response)):
+                state.isPoseRequest = false
+                state.tempResult = response
+                return .none
+                
+            case .poseResponse(.failure):
+                state.isPoseRequest = false
                 return .none
             }
         }
