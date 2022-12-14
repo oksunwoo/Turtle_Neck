8c4d4998e0ceea6854eaa96001e8232a85e1df3e
oksunwoo <oksunwo@naver.com>
2022-12-14 21:03:23 +0900

    refactor: 각 부위에 점을 찍을 수 있는 메서드 작성

--- TurtleNeck/TurtleNeck/Utility/DotMaker.swift
+++ TurtleNeck/TurtleNeck/Utility/DotMaker.swift
@@ -8,97 +8,62 @@
 import Foundation
 import UIKit
 
-    func addDot(on image: UIImage, with pose: [Pose]) -> UIImage {
-        guard let pose = pose.first else {
-            return image
-        }
-        
-        UIGraphicsBeginImageContextWithOptions(image.size, true, 0)
-        image.draw(at: CGPointMake(0, 0))
-
-        let bbox = pose.bbox
-        let keypoints = pose.keypoints
-        let context = UIGraphicsGetCurrentContext()!
-        let rectangle = CGRectMake(bbox[0], bbox[1], bbox[2], bbox[3])
-        
-        context.setLineWidth(4.0)
-        context.setStrokeColor(UIColor.systemYellow.cgColor)
-        context.addRect(rectangle)
-        context.addEllipse(in: CGRect(x: keypoints[0], y: keypoints[1], width: 10, height: 10))
-        context.strokePath()
-        
-       let image = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
+func addDot(on image: UIImage, with pose: [Pose]) -> UIImage {
+    guard let pose = pose.first else {
         return image
     }
-
-
     
+    UIGraphicsBeginImageContextWithOptions(image.size, true, 0)
+    image.draw(at: CGPointMake(0, 0))
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let bbox = pose.bbox
+    let keypoints = pose.keypoints
     
+    let nose = 0
+    let leftEye = 3
+    let rightEye = 6
+    let leftEar = 9
+    let rightEar = 12
+    let leftShoulder = 15
+    let rightShoulder = 18
+    let leftElbow = 21
+    let rightElbow = 24
+    let leftWrist = 27
+    let rightWrist = 30
+    let leftHip = 33
+    let rightHip = 36
+    let leftKnee = 39
+    let rightKnee = 42
+    let leftAnkle = 45
+    let rightAnkle = 48
     
-//    func addDot(on pose: Pose) -> UIImage {
-//            UIGraphicsBeginImageContextWithOptions(size, false, 0)
-//            self.draw(at: CGPointMake(0,0))
-//
-//            let bbox = pose.bbox
-//            let keyPoint = pose.keypoints
-//
-//            let leftEar = 9
-//            let leftShoulder = 15
-//            let leftHip = 33
-//            let leftAnkle = 45
-//
-//            let context = UIGraphicsGetCurrentContext()!
-//            let rectangle = CGRectMake(bbox[0], bbox[1], bbox[2], bbox[3])
-//
-//            context.setLineWidth(2.0)
-//            context.setStrokeColor(UIColor.red.cgColor)
-//
-//            context.addRect(rectangle)
-//
-//            // 귀
-//            context.addEllipse(in: CGRect(x: keyPoint[leftEar], y: keyPoint[leftEar + 1], width: 10, height: 10))
-//
-//            // 어깨
-//            context.addLine(to: CGPoint(x: keyPoint[leftShoulder], y: keyPoint[leftShoulder + 1]))
-//            context.addEllipse(in: CGRect(x: keyPoint[leftShoulder], y: keyPoint[leftShoulder + 1], width: 10, height: 10))
-//
-//            // 엉덩이
-//            context.addLine(to: CGPoint(x: keyPoint[leftHip], y: keyPoint[leftHip + 1]))
-//            context.addEllipse(in: CGRect(x: keyPoint[leftHip], y: keyPoint[leftHip + 1], width: 10, height: 10))
-//
-//            // 발목
-//            context.addLine(to: CGPoint(x: keyPoint[leftAnkle], y: keyPoint[leftAnkle + 1]))
-//            context.addEllipse(in: CGRect(x: keyPoint[leftAnkle], y: keyPoint[leftAnkle + 1], width: 10, height: 10))
-//            context.strokePath()
-//
-//            let image = UIGraphicsGetImageFromCurrentImageContext()!
-//            UIGraphicsEndImageContext()
-//
-//            return image
-//        }
-
+    let context = UIGraphicsGetCurrentContext()!
+    let rectangle = CGRectMake(bbox[0], bbox[1], bbox[2], bbox[3])
+    
+    context.setLineWidth(4.0)
+    context.setStrokeColor(UIColor.systemYellow.cgColor)
+    context.addRect(rectangle)
+    context.addEllipse(in: CGRect(x: keypoints[nose], y: keypoints[nose + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[leftEye], y: keypoints[leftEye + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[rightEye], y: keypoints[rightEye + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[leftEar], y: keypoints[leftEar + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[rightEar], y: keypoints[rightEar + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[leftShoulder], y: keypoints[leftShoulder + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[rightShoulder], y: keypoints[rightShoulder + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[leftElbow], y: keypoints[leftElbow + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[rightElbow], y: keypoints[rightElbow + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[leftWrist], y: keypoints[leftWrist + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[rightWrist], y: keypoints[rightWrist + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[leftHip], y: keypoints[leftHip + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[rightHip], y: keypoints[rightHip + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[leftKnee], y: keypoints[leftKnee + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[rightKnee], y: keypoints[rightKnee + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[leftAnkle], y: keypoints[leftAnkle + 1], width: 5, height: 5))
+    context.addEllipse(in: CGRect(x: keypoints[rightAnkle], y: keypoints[rightAnkle + 1], width: 5, height: 5))
+    context.strokePath()
+    
+    let image = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    
+    return image
+}
