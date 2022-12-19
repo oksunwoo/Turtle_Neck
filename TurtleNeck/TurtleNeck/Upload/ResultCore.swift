ad26d1c2deb97481399f57dde21cecdc76c220c7
oksunwoo <oksunwo@naver.com>
2022-12-19 18:46:54 +0900

    feat: resultCore 구현

---
+++ TurtleNeck/TurtleNeck/Upload/ResultCore.swift
@@ -0,0 +1,28 @@
+//
+//  ResultCore.swift
+//  TurtleNeck
+//
+//  Created by Sunwoo on 2022/12/19.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ResultCore: ReducerProtocol {
+    struct State: Equatable {
+        var resultImage: UIImage?
+        var pose: [Pose]?
+    }
+    
+    enum Action: Equatable {
+        case dotButtonTapped
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .dotButtonTapped:
+            state.resultImage = addDot(on: state.resultImage!, with: state.pose!)
+            return .none
+        }
+    }
+}
