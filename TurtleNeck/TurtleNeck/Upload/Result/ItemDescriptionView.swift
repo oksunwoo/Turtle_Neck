4f455ccba74766e6f7e1728561b5aea95ad15e99
oksunwoo <oksunwo@naver.com>
2022-12-30 18:14:00 +0900

    refactor: ItemDescriptionView를 인자에 따라 다른 뷰를 그리도록 수정

--- TurtleNeck/TurtleNeck/Upload/Result/ItemDescriptionView.swift
+++ TurtleNeck/TurtleNeck/Upload/Result/ItemDescriptionView.swift
@@ -8,22 +8,33 @@
 import SwiftUI
 
 struct ItemDescriptionView: View {
+    var summary: Summary?
+    var score: Double?
+    var degree: Double?
+    
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(0..<5, id: \.self) { _ in
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.yellow)
+        if score == nil {
+            Text("현재 목의 각도가 \(Int(round(degree!)))º 기울어져 있습니다.")
+        }
+        
+        if degree == nil {
+            VStack {
+                HStack {
+                    ForEach(0..<summary!.star, id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                    }
                 }
+                .padding()
+                Text(summary!.shortDescription)
+                    .bold()
             }
-            .padding(10)
-            Text("훌륭합니다")
         }
     }
 }
 
 struct ItemDescriptionView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemDescriptionView()
+        ItemDescriptionView(summary: Summary.perfect, score: 30)
     }
 }
