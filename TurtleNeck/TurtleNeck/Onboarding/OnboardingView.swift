4663ef16506711d448a05f6ff09729e0a4fd1f2d
zoe <kny0651@naver.com>
2022-12-30 17:27:11 +0900

    feat: 온보딩뷰 애니메이션 추가

--- TurtleNeck/TurtleNeck/Onboarding/OnboardingView.swift
+++ TurtleNeck/TurtleNeck/Onboarding/OnboardingView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var animate = false
     @Binding var isFirstLaunching: Bool
     @Environment(\.presentationMode) var presentationMode
     
@@ -18,19 +19,27 @@ struct OnboardingView: View {
             VStack(spacing: 20) {
                 Image("TurtleNeck")
                     .resizable()
+                    .opacity(animate ? 1 : 0)
+                    .offset(y: animate ? 0 : 100)
                     .frame(width: 200, height: 200)
+                    .animation(.easeOut(duration: 0.6).delay(0.4), value: animate)
                 
-                Text("거북목측정하세요")
-                    .bold()
-                    .font(.title)
+                Text("거북목 측정하세요")
                     .foregroundColor(.white)
+                    .font(.title.weight(.bold))
+                    .opacity(animate ? 1 : 0)
+                    .offset(y: animate ? 0 : 100)
                     .multilineTextAlignment(.center)
+                    .animation(.easeOut(duration: 0.6).delay(0.6), value: animate)
                 
                 Text("사진인식 후 귀, 어깨, 허리, 엉덩이 등 \n총 17개의 키포인트를 추출하여 자세를 분석해드립니다")
                     .font(.body)
                     .foregroundColor(.white)
-                    .foregroundColor(.black.opacity(0.5))
+                    .opacity(animate ? 1 : 0)
+                    .offset(y: animate ? 0 : 100)
                     .multilineTextAlignment(.center)
+                    .foregroundColor(.black.opacity(0.5))
+                    .animation(.easeOut(duration: 0.6).delay(0.8), value: animate)
                 
                 Button {
                     isFirstLaunching = false
@@ -46,8 +55,14 @@ struct OnboardingView: View {
                         )
                 }
                 .padding(.top, 30)
+                .opacity(animate ? 1 : 0)
+                .offset(y: animate ? 0 : 100)
+                .animation(.easeOut(duration: 0.6).delay(1.5), value: animate)
             }
             .padding()
+            .onAppear {
+                animate.toggle()
+            }
         }
     }
 }
