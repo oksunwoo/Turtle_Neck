6caaf5071d77273fb73cab3faf0b5d55a106679a
zoe <kny0651@naver.com>
2023-01-03 17:51:43 +0900

    feat: 코어데이터를위한 User 데이터모델 생성

---
+++ TurtleNeck/TurtleNeck/CoreData/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  TurtleNeck
+//
+//  Created by 권나영 on 2023/01/03.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
---
+++ TurtleNeck/TurtleNeck/CoreData/User+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  User+CoreDataProperties.swift
+//  TurtleNeck
+//
+//  Created by 권나영 on 2023/01/03.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var image: Data?
+    @NSManaged public var score: Int16
+    @NSManaged public var degree: Int16
+    @NSManaged public var date: Date?
+    @NSManaged public var validity: Double
+
+}
+
+extension User : Identifiable {
+
+}
