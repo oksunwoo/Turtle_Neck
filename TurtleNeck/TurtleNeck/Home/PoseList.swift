c64456d4c4a36fa5a023562086ad81af66852cd9
zoe <kny0651@naver.com>
2023-01-09 16:07:14 +0900

    feat: 저장된데이터가 없을때 보여줄 empty뷰 추가

--- TurtleNeck/TurtleNeck/Home/PoseList.swift
+++ TurtleNeck/TurtleNeck/Home/PoseList.swift
@@ -20,32 +20,44 @@ struct PoseList: View {
 
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack(alignment: .leading, spacing: 30) {
-                Text("지난 기록들")
-                    .font(.headline)
-                    .padding(.leading, 30)
-                    .padding(.top, 5)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(alignment: .top, spacing: 0) {
-                        ForEach(userData) { data in
-                            
-                            NavigationLink(destination:  IfLetStore(self.store.scope(state: \.optionalResult, action: HomeCore.Action.optionalResult)) {
-                                ResultView(store: $0)
+        
+            if userData.count == 0 {
+                VStack(alignment: .center) {
+                    Image("Pose3")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .padding(.vertical, 30)
+                    
+                    Text("분석을 통해 내가 거북목인지 알아보세요!")
+                }
+            } else {
+                VStack(alignment: .leading, spacing: 30) {
+                    Text("지난 기록들")
+                        .font(.headline)
+                        .padding(.leading, 30)
+                        .padding(.top, 5)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .top, spacing: 0) {
+                            ForEach(userData) { data in
                                 
-                            }, isActive: viewStore.binding(get: \.isNavigationActive, send: HomeCore.Action.poseItemTapped(isNavigationActive: true, data: data))) {
-                                
-                                // label
-                                PoseItem(
-                                    image: UIImage(data: data.image!) ?? UIImage(),
-                                    score: data.score,
-                                    grade: Grade(score: Int(data.score))
-                                )
+                                NavigationLink(destination:  IfLetStore(self.store.scope(state: \.optionalResult, action: HomeCore.Action.optionalResult)) {
+                                    ResultView(store: $0)
+                                    
+                                }, isActive: viewStore.binding(get: \.isNavigationActive, send: HomeCore.Action.poseItemTapped(isNavigationActive: true, data: data))) {
+                                    
+                                    // label
+                                    PoseItem(
+                                        image: UIImage(data: data.image!) ?? UIImage(),
+                                        score: data.score,
+                                        grade: Grade(score: Int(data.score))
+                                    )
+                                }
                             }
                         }
                     }
+                    .frame(height: 185)
                 }
-                .frame(height: 185)
             }
         }
     }
