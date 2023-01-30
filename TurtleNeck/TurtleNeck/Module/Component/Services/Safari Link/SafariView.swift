37c2f82a1ba3946146a5a09b6b38d3c9be117dbe
oksunwoo <oksunwo@naver.com>
2023-01-30 19:18:21 +0900

    feat: 사파리를 띄울수 있는 사파리뷰 생성

---
+++ TurtleNeck/TurtleNeck/Module/Component/Services/Safari Link/SafariView.swift
@@ -0,0 +1,21 @@
+//
+//  SafariView.swift
+//  TurtleNeck
+//
+//  Created by Sunwoo on 2023/01/30.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+}
