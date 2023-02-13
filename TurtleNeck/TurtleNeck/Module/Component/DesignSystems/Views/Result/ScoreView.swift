dfeaad3c82bf2f75abf89cb595481afb7ead8b3c
oksunwoo <oksunwo@naver.com>
2023-02-13 23:01:03 +0900

    refactor: 결과화면 변경

--- TurtleNeck/TurtleNeck/Module/Component/DesignSystems/Views/Result/ScoreView.swift
+++ TurtleNeck/TurtleNeck/Module/Component/DesignSystems/Views/Result/ScoreView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ScoreView: View {
     let kilogram: Int
+    let degree: Int
     
     var body: some View {
-        VStack {
+        VStack(spacing: 40) {
             ZStack {
                 ProgressBar(kilogram: CGFloat(kilogram))
                 KilogramBar()
-                    .offset(y: 5)
+                    .offset(y: 8)
+            }
+            ZStack {
+                ProgressView(value: Double(degree), total: 50)
+                    .progressViewStyle(ResultProgressStyle(degree: Double(degree)))
+                    .frame(width: 150, height: 150)
+                    .contentShape(Rectangle())
+                HStack(spacing: 2) {
+                    RollingText(value: degree)
+                    Text("°")
+                        .offset(y: -10)
+                }
             }
         }
     }
@@ -23,6 +35,6 @@ struct ScoreView: View {
 
 struct ScoreView_Previews: PreviewProvider {
     static var previews: some View {
-        ScoreView(kilogram: 5)
+        ScoreView(kilogram: 5, degree: 45)
     }
 }
